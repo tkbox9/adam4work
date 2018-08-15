8785bf4a72757b7d77d7f2c9247fcd590cc25a6a
tkbox9@gmail.com <wolf@wolfsmacbook.mynet>
2018-08-15 20:13:19 +0100

    add cards

--- app.js
+++ app.js
@@ -62,34 +62,54 @@ var AudioCardName = "Audio card";
 var CardNames = [HeroCardName, ThumbnailCardName, ReceiptCardName, SigninCardName, AnimationCardName, VideoCardName, AudioCardName];
 
 
-function createCard(selectedCardName, session) {
-    switch (selectedCardName) {
-        case HeroCardName:
-            return createHeroCard(session);
-        default:
-            return createHeroCard(session);
-    }
-}
-    function createHeroCard(session) {
-        return new builder.HeroCard(session)
-            .title('BotFramework Hero Card')
-            .subtitle('Your bots — wherever your users are talking')
-            .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+function createCards(session) {
+    return [
+        new builder.HeroCard(session)
+            .title('Azure Storage')
+            .subtitle('Offload the heavy lifting of data center management')
+            .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
             .images([
-                builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/aspnet/aspnet/overview/developing-apps-with-windows-azure/building-real-world-cloud-apps-with-windows-azure/data-storage-options/_static/image5.png')
             ])
             .buttons([
-                builder.CardAction.openUrl(session, 'https://docs.microsoft.com/bot-framework/', 'Get Started')
-            ]);
-    }
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+            ]),
 
+        new builder.ThumbnailCard(session)
+            .title('DocumentDB')
+            .subtitle('Blazing fast, planet-scale NoSQL')
+            .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
+            .images([
+                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/documentdb/media/documentdb-introduction/json-database-resources1.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
+            ]),
 
+        new builder.HeroCard(session)
+            .title('Azure Functions')
+            .subtitle('Process events with a serverless code architecture')
+            .text('An event-based serverless compute experience to accelerate your development. It can scale based on demand and you pay only for the resources you consume.')
+            .images([
+                builder.CardImage.create(session, 'https://msdnshared.blob.core.windows.net/media/2016/09/fsharp-functions2.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
+            ]),
 
-
-
-
-
-
+        new builder.ThumbnailCard(session)
+            .title('Cognitive Services')
+            .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
+            .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
+            .images([
+                builder.CardImage.create(session, 'https://msdnshared.blob.core.windows.net/media/2017/03/Azure-Cognitive-Services-e1489079006258.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/cognitive-services/', 'Learn More')
+            ])
+    ];
+}
+   
 
 
 // Add a dialog for each intent that the LUIS app recognizes.
@@ -97,11 +117,8 @@ function createCard(selectedCardName, session) {
 bot.dialog('GreetingDialog',
     (session) => {
 
-        var card = createCard("HeroCardName", session);
-        var msg = new builder.Message(session).addAttachment(card);
-        msg.addAttachment(card);
-        msg.addAttachment(card);
-        msg.addAttachment(card);
+        var cards = createCards(session);
+        var msg = new builder.Message(session).addAttachment(cards);
 
         session.send(msg);
        // session.send('Adam: You reached the Greeting intent. You said \'%s\'.', session.message.text);
